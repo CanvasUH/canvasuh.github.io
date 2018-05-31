9741e496e0197f9b5ad6fb7aa06df42019333d1a
Tore B. Jørgensen <t.b.jorgensen@link.uio.no>
2018-05-31 11:01:00 +0200

    Update gotoaccount.js
    Inserted the script.

--- admin/gotoaccount/gotoaccount.js
+++ admin/gotoaccount/gotoaccount.js
@@ -1,1 +1,16 @@
+// ==UserScript==
+// @name         gotoaccount
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add jump to subaccount-button in settingspage
+// @author       Tore
+// @match        https://*.instructure.com/courses/*/settings
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+        $.getJSON('/api/v1/courses/'+ENV.COURSE_ID,function (data) {
+            $("#right-side table.summary").before("<a class='Button Button--link Button--link--has-divider Button--course-settings' href='/accounts/"+data.account_id+"'><i class='icon-replied' />Jump to account</a>");
+        });
+})();
