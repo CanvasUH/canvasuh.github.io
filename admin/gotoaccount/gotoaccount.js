78bde7453738b60d327a664568d68d961baad981
Tore B. Jørgensen <t.b.jorgensen@link.uio.no>
2020-03-02 10:57:40 +0100

    Update gotoaccount.js
    Removed jquery dependencies.

--- admin/gotoaccount/gotoaccount.js
+++ admin/gotoaccount/gotoaccount.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         gotoaccount
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Add jump to subaccount-button in settingspage
 // @author       Tore
 // @match        https://*.instructure.com/courses/*/settings
@@ -10,7 +10,15 @@
 
 (function() {
     'use strict';
-        $.getJSON('/api/v1/courses/'+ENV.COURSE_ID,function (data) {
-            $("#right-side table.summary").before("<a class='Button Button--link Button--link--has-divider Button--course-settings' href='/accounts/"+data.account_id+"'><i class='icon-replied' />Jump to account</a>");
-        });
+    async function CanvasGet (last_part_of_url){
+        //Does a Canvas API call and returns a JS-object.
+        let response = await fetch(location.origin+'/api/v1/'+last_part_of_url);
+        let data = await response.text();
+        return JSON.parse(data.substring(9));
+    }
+    CanvasGet('courses/'+ENV.COURSE_ID).then((data)=>{
+        let newbutton = document.createElement('a');
+        document.querySelector("#right-side table.summary").before(newbutton);
+        newbutton.outerHTML = "<a class='Button Button--link Button--link--has-divider Button--course-settings' href='/accounts/"+data.account_id+"'><i class='icon-replied' /> Jump to account</a>";
+    });
 })();
